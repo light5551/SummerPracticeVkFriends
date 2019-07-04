ad0bf18e60451194a4664ffd1117d4689df4fb3d
light5551 <sergey.glazunov.99@mail.ru>
2019-07-04 22:44:27 +0300

    добавлено содержимое файлов

--- src/VkApi/VKUser.java
+++ src/VkApi/VKUser.java
@@ -1,4 +1,30 @@
 package VkApi;
 
 public class VKUser {
+    public int userId;
+    public String firstName;
+    public String lastName;
+    /*public boolean is_closed;
+    public boolean can_access_closed;
+    public int sex;
+    public String bdate;
+    public String university_name;
+    public String faculty_name;
+    public Place city;
+    public Place country; */
+
+    public VKUser(
+            int userId,
+            String firstName,
+            String lastName)
+    {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return userId + " " + firstName + " " + lastName;
+    }
 }
--- src/VkApi/VkAPI.java
+++ src/VkApi/VkAPI.java
@@ -1,4 +1,80 @@
 package VkApi;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.net.URL;
+import java.net.URLConnection;
+
+import com.google.gson.*;
+
 public class VkAPI {
+
+    private final String accessVkApiToken = "<key of app>";
+    private final String versionVkApi = "5.101";
+    private final String beginVkApi = "https://api.vk.com/method/";
+    private final String endVkApi = "&access_token=" + accessVkApiToken + "&v=" + versionVkApi;
+
+    public ArrayList<VKUser> parseFriendsJson(String str) {
+        try
+        {
+            JsonParser jsonParser = new JsonParser();
+            JsonObject jo = (JsonObject)jsonParser.parse(str);
+            if (jo.get("error") != null)
+                return null;
+            JsonArray jsonArr = jo.get("response").getAsJsonObject().getAsJsonArray("items");
+            ArrayList<VKUser> list = new ArrayList<>();
+
+            for (JsonElement obj : jsonArr)
+            {
+                JsonObject tmp = obj.getAsJsonObject();
+                list.add( new VKUser( tmp.get("id").getAsInt(),
+                        tmp.get("first_name").getAsString(),
+                        tmp.get("last_name").getAsString()));
+            }
+
+            return list;
+        }
+        catch (Exception e)
+        {
+            System.out.println("Failed: parseFriendsJson");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getUserFriends(int userId, String[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append(beginVkApi + "friends.get?user_id=" + userId);
+        if(args != null) {
+            sb.append("&fields=");
+            for (String field : args)
+                sb.append(field + ",");
+            sb.deleteCharAt(sb.length()-1);
+        }
+        sb.append(endVkApi);
+
+
+        return getRequest(sb.toString());
+    }
+
+    private String getRequest(String url)
+    {
+        StringBuilder sb = new StringBuilder();
+        try {
+            URL requestUrl=new URL(url);
+            URLConnection con = requestUrl.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            int cp;
+            while ((cp = in.read()) != -1) {
+                sb.append((char) cp);
+            }
+        }
+        catch(Exception e) {
+            System.out.println("Failed: getRequest");
+        }
+        return sb.toString();
+    }
+
 }
