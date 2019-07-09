58887438172acd2b42b00f2b5e5d2b0fa3497b79
light5551 <sergey.glazunov.99@mail.ru>
2019-07-09 19:54:23 +0300

    добавлена функция быстрого получения списка пользователя

--- src/VkApi/VkAPI.java
+++ src/VkApi/VkAPI.java
@@ -10,7 +10,7 @@ import com.google.gson.*;
 
 public class VkAPI {
 
-    private final String accessVkApiToken = "<key of app>";
+    private final String accessVkApiToken = "0c91fdfc0c91fdfc0c91fdfc3e0cfa8e5100c910c91fdfc518a61b257703bf5b0589264";
     private final String versionVkApi = "5.101";
     private final String beginVkApi = "https://api.vk.com/method/";
     private final String endVkApi = "&access_token=" + accessVkApiToken + "&v=" + versionVkApi;
@@ -110,6 +110,12 @@ public class VkAPI {
             System.out.println("Failed: getRequest");
         }
         return sb.toString();
+    }
+
+    public ArrayList<VKUser> getFriends(int userId,  String order, String[] args)
+    {
+        String response = getUserFriends(userId, order, args);
+        return parseFriendsJson(response);
     }
 
 }
