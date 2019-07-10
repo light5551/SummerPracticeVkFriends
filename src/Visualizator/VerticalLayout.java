0cd0d3bb73094a4641578544e982ff89c683a542
yawningstudent <andr.tok2012@mail.ru>
2019-07-10 21:59:34 +0300

    added some functions, have bugs

---
+++ src/Visualizator/VerticalLayout.java
@@ -0,0 +1,54 @@
+package Visualizator;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
+
+// Менеджер вертикального расположения компонентов
+class VerticalLayout implements LayoutManager
+{
+    private Dimension size = new Dimension();
+
+    public void addLayoutComponent   (String name, Component comp) {}
+    public void removeLayoutComponent(Component comp) {}
+
+    public Dimension minimumLayoutSize(Container c) {
+        return calculateBestSize(c);
+    }
+
+    public Dimension preferredLayoutSize(Container c) {
+        return calculateBestSize(c);
+    }
+
+    public void layoutContainer(Container container)
+    {
+        Component list[] = container.getComponents();
+        int currentY = 5;
+        for (int i = 0; i < list.length; i++) {
+            Dimension pref = list[i].getPreferredSize();
+            list[i].setBounds(5, currentY, pref.width, pref.height);
+            currentY += 5;
+            currentY += pref.height;
+        }
+    }
+
+    private Dimension calculateBestSize(Container c)
+    {
+        Component[] list = c.getComponents();
+        int maxWidth = 0;
+        for (int i = 0; i < list.length; i++) {
+            int width = list[i].getWidth();
+            if ( width > maxWidth )
+                maxWidth = width;
+        }
+        size.width = maxWidth + 5;
+        int height = 0;
+        for (int i = 0; i < list.length; i++) {
+            height += 5;
+            height += list[i].getHeight();
+        }
+        size.height = height;
+        return size;
+    }
+}
--- src/Visualizator/Visualizator.java
+++ src/Visualizator/Visualizator.java
@@ -9,56 +9,267 @@ import java.util.ArrayList;
 import Main.Main;
 import VkApi.VkAPI;
 import VkApi.VKUser;
+import java.awt.Font;
+import java.awt.event.*;
 
 public class Visualizator extends JFrame {
 
+    static int userID;
+    static int friendID;
+    //final int userID = 179878269;
     final int xRoot = 600, yRoot = 20, xBorder = 20;
+    mxGraphComponent graphComponent;
+    JTextField userField;
+    JTextField friendField;
+    JPanel mainPanel;
+    JScrollPane scrollPane = new JScrollPane(graphComponent);
+    JPanel checkPanel = new JPanel();
+    JPanel exitPanel = new JPanel();
 
     public Visualizator() {
 
         super("Visualizator"); //call jframe constructor
 
-        mxGraph friendsGraph = new mxGraph();
-            Object parent = friendsGraph.getDefaultParent();
-            friendsGraph.getModel().beginUpdate();
-            try {
-                    ArrayList<VKUser> friendList = Main.getList();
-                    VKUser currentUser = VkAPI.getCurrentUser();
-                    String in =  currentUser.firstName + " " + currentUser.lastName;
-                    ArrayList vertexes = new ArrayList();
-                    vertexes.add(friendsGraph.insertVertex(parent, null, in, xRoot, yRoot, 80, 30));
-                    int y = -1, x = -1;
-                    for (int i = 0; i<friendList.size(); ){
-                        mxCell c = (mxCell)vertexes.get(i);
-                        in = friendList.get(i).firstName + " " + friendList.get(i).lastName;
-                        if(c.getValue().toString().charAt(0) == in.charAt(0)) {
-                            if(i==0) x++;
-                            vertexes.add(friendsGraph.insertVertex(parent, null, in, xBorder + x * 130,  yRoot + 80 + (y + 1)* 50, 80, 30));
-                            i++;
-                            y++;
-                            friendsGraph.insertEdge(parent, null, "", vertexes.get(i - 1), vertexes.get(i));
-                        }
-                        else{
-                            y = 0;
-                            x++;
-                            vertexes.add(friendsGraph.insertVertex(parent, null, in,  xBorder + x * 130, yRoot + 80 + y * 50, 80, 30));
-                            i++;
-                            friendsGraph.insertEdge(parent, null, "", vertexes.get(0), vertexes.get(i));
-                        }
-                    }
-            } finally {
-                friendsGraph.getModel().endUpdate();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        mainPanel = new JPanel(new VerticalLayout());
+        JPanel interfacePanel = initInterface();
+        mainPanel.add(interfacePanel);
+        setContentPane(mainPanel);
+        setPreferredSize(new Dimension(400,400));
+        setLocation(550, 200);
+        pack();
+        setVisible(true);
+
+    }
+
+    public JPanel initInterface (){
+
+        userField = new JTextField("Введите ID", 15);
+        userField.setToolTipText("Введите ID");
+        userField.setFont(new Font("Dialog", Font.PLAIN, 14));
+        userField.setHorizontalAlignment(JTextField.LEFT);
+
+        friendField = new JTextField("Введите ID друга", 20);
+        friendField.setToolTipText("Введите ID друга");
+        friendField.setFont(new Font("Dialog", Font.PLAIN, 14));
+        friendField.setHorizontalAlignment(JTextField.LEFT);
+
+        JPanel interfacePanel = new JPanel(new FlowLayout());
+        interfacePanel.add(userField);
+
+        JButton buildButton = new JButton("Построить граф");
+        buildButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                userID = Integer.parseInt(userField.getText());
+                initGraphPanel();
+                mainPanel.updateUI();
             }
-            mxGraphComponent graphComponent = new mxGraphComponent(friendsGraph);
-            getContentPane().add(graphComponent);
+        });
+
+        JButton buildCommonButton = new JButton("Построить граф общих друзей");
+        buildCommonButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                friendID = Integer.parseInt(friendField.getText());
+                clearPanel(scrollPane);
+                clearPanel(exitPanel);
+                clearPanel(checkPanel);
+                initCommonFriendsGraph();
+                mainPanel.updateUI();
+            }
+        });
+
+        interfacePanel.add(buildButton);
+        interfacePanel.add(friendField);
+        interfacePanel.add(buildCommonButton);
+        return interfacePanel;
+    }
+
+
+    public void initGraphPanel () {
+
+        mxGraph friendsGraph = new mxGraph();
+        Object parent = friendsGraph.getDefaultParent();
+        friendsGraph.getModel().beginUpdate();
+        try {
+            ArrayList<VKUser> friendList = Main.getList(userID);
+            VKUser currentUser = VkAPI.getCurrentUser();
+            String in = currentUser.firstName + " " + currentUser.lastName;
+            ArrayList vertexes = new ArrayList();
+            String ava = "shape=image;image="+currentUser.urlImage_50+";verticalLabelPosition=bottom";
+            vertexes.add(friendsGraph.insertVertex(parent, null, in, xRoot, yRoot, 80, 40, ava));
+            int y = -1, x = -1;
+            for (int i = 0; i < friendList.size(); ) {
+                mxCell c = (mxCell) vertexes.get(i);
+                in = friendList.get(i).firstName + " " + friendList.get(i).lastName;
+                ava = "shape=image;image="+friendList.get(i).urlImage_50+";verticalLabelPosition=bottom";
+                if (c.getValue().toString().charAt(0) == in.charAt(0)) {
+                    if (i == 0) x++;
+                    vertexes.add(friendsGraph.insertVertex(parent, null, in,  xBorder + x * 130, yRoot + 80 + (y + 1) * 70, 80, 40, ava));
+                    i++;
+                    y++;
+                    friendsGraph.insertEdge(parent, null, "", vertexes.get(i - 1), vertexes.get(i));
+                } else {
+                    y = 0;
+                    x++;
+                    vertexes.add(friendsGraph.insertVertex(parent, null, in, xBorder + x * 130, yRoot + 80 + y * 70, 80, 40, ava));
+                    i++;
+                    friendsGraph.insertEdge(parent, null, "", vertexes.get(0), vertexes.get(i));
+                }
+            }
+
+        } finally {
+            friendsGraph.getModel().endUpdate();
         }
+        mxGraphComponent graphComponent = new mxGraphComponent(friendsGraph);
+        graphComponent.getViewport().setOpaque(true);
+        Color newColor = new Color(192, 192, 192);
+        graphComponent.getViewport().setBackground(newColor);
+        scrollPane = new JScrollPane(graphComponent);
+        checkPanel = new JPanel();
+        //scrollPane.setMaximumSize(new Dimension(800, 500));
+        exitPanel = new JPanel();
+        JButton exitButton = new JButton("Назад");
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearPanel(scrollPane);
+                clearPanel(exitPanel);
+                clearPanel(checkPanel);
+            }
+        });
+        exitPanel.add(exitButton);
+        mainPanel.add(exitPanel);
+        mainPanel.add(scrollPane, BorderLayout.WEST);
+        final JCheckBox checkBox1 = new JCheckBox("Show vertical scrollbar");
+        checkBox1.setSelected(true);
+        checkBox1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (checkBox1.isSelected()) {
+                    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+                } else {
+                    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+                }
+            }
+        });
+        checkPanel.add(checkBox1);
+
+        final JCheckBox checkBox2 = new JCheckBox("Show horizontal scrollbar");
+        checkBox2.setSelected(true);
+        checkBox2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (checkBox2.isSelected()) {
+                    scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+                } else {
+                    scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                }
+            }
+        });
+        checkPanel.add(checkBox2);
+
+        mainPanel.add(checkPanel, BorderLayout.SOUTH);
+    }
+
+    public void clearPanel(Component comp){
+        mainPanel.remove(comp);
+        mainPanel.updateUI();
+    }
+
+    public void initCommonFriendsGraph(){
+
+        mxGraph CommonfriendsGraph = new mxGraph();
+        Object parent = CommonfriendsGraph.getDefaultParent();
+        CommonfriendsGraph.getModel().beginUpdate();
+        try {
+            VkAPI vk = new VkAPI();
+            VKUser currentUser = VkAPI.getCurrentUser();
+            String in = currentUser.firstName + " " + currentUser.lastName;
+            ArrayList vertexes = new ArrayList();
+            String ava = "shape=image;image="+currentUser.urlImage_50+";verticalLabelPosition=bottom";
+            vertexes.add(CommonfriendsGraph.insertVertex(parent, null, in, xRoot, yRoot, 80, 40, ava));
+            vk.updateCurrentUser(friendID);
+            VKUser currentFriend = VkAPI.getCurrentUser();
+            ArrayList<VKUser> friendList = vk.getCommonFriends(userID, friendID);
+            ava = "shape=image;image="+currentFriend.urlImage_50+";verticalLabelPosition=bottom";
+            in = currentFriend.firstName + " " + currentFriend.lastName;
+            vertexes.add(CommonfriendsGraph.insertVertex(parent, null, in, xRoot + 100, yRoot, 80, 40, ava));
+            Object firstEdge = CommonfriendsGraph.insertEdge(parent, null, "", vertexes.get(0), vertexes.get(1));
+            int y = -1, x = -1;
+            for (int i = 1; i < friendList.size() + 1; ) {
+                mxCell c = (mxCell) vertexes.get(i);
+                in = friendList.get(i-1).firstName + " " + friendList.get(i-1).lastName;
+                ava = "shape=image;image="+friendList.get(i-1).urlImage_50+";verticalLabelPosition=bottom";
+                if (c.getValue().toString().charAt(0) == in.charAt(0)) {
+                    if (i == 0) x++;
+                    vertexes.add(CommonfriendsGraph.insertVertex(parent, null, in,  xBorder + x * 130, yRoot + 80 + (y + 1) * 70, 80, 40, ava));
+                    i++;
+                    y++;
+                    CommonfriendsGraph.insertEdge(parent, null, "", vertexes.get(i - 1), vertexes.get(i));
+                } else {
+                    y = 0;
+                    x++;
+                    vertexes.add(CommonfriendsGraph.insertVertex(parent, null, in, xBorder + x * 130, yRoot + 80 + y * 70, 80, 40, ava));
+                    i++;
+                    CommonfriendsGraph.insertEdge(parent, null, "", firstEdge, vertexes.get(i));
+                }
+            }
+
+        } finally {
+            CommonfriendsGraph.getModel().endUpdate();
+        }
+        mxGraphComponent graphComponent = new mxGraphComponent(CommonfriendsGraph);
+        graphComponent.getViewport().setOpaque(true);
+        Color newColor = new Color(192, 192, 192);
+        graphComponent.getViewport().setBackground(newColor);
+        final JScrollPane scrollPane = new JScrollPane(graphComponent);
+        JPanel checkPanel = new JPanel();
+        //scrollPane.setMaximumSize(new Dimension(800, 500));
+        JPanel exitPanel = new JPanel();
+        JButton exitButton = new JButton("Назад");
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearPanel(scrollPane);
+                clearPanel(exitPanel);
+                clearPanel(checkPanel);
+            }
+        });
+        exitPanel.add(exitButton);
+        mainPanel.add(exitPanel);
+        mainPanel.add(scrollPane, BorderLayout.WEST);
+        final JCheckBox checkBox1 = new JCheckBox("Show vertical scrollbar");
+        checkBox1.setSelected(true);
+        checkBox1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (checkBox1.isSelected()) {
+                    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+                } else {
+                    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+                }
+            }
+        });
+        checkPanel.add(checkBox1);
+
+        final JCheckBox checkBox2 = new JCheckBox("Show horizontal scrollbar");
+        checkBox2.setSelected(true);
+        checkBox2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (checkBox2.isSelected()) {
+                    scrollPane
+                            .setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+                } else {
+                    scrollPane
+                            .setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                }
+            }
+        });
+        checkPanel.add(checkBox2);
+
+        mainPanel.add(checkPanel, BorderLayout.SOUTH);
+    }
 
     public static void initGUI(){
         Visualizator frame = new Visualizator();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(1000,600));
-        frame.setLocation(200, 100);
-        frame.pack();
-        frame.setVisible(true);
     }
 }
